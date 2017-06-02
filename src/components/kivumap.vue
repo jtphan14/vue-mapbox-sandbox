bc8653467250c8c24f599e04e7474ed2bd0a3add
Jacqueline <jtphan14@gmail.com>
2017-06-02 14:15:14 -0700

    Add geojson sample data layer

--- src/components/kivumap.vue
+++ src/components/kivumap.vue
@@ -15,6 +15,63 @@
     },
     components: {
       'mapbox': Mapbox
+    },
+    methods: {
+      mapLoaded(map) {
+        console.log('Map Loaded!')
+
+        map.addLayer({
+          'id': 'points',
+          'type': 'symbol',
+          'source': {
+            'type': 'geojson',
+            'data': {
+              'type': 'FeatureCollection',
+              'features': [{
+                'type': 'Feature',
+                'geometry': {
+                  'type': 'Point',
+                  'coordinates': [-77.03238901390978, 38.913188059745586]
+                },
+                'properties': {
+                  'title': 'Mapbox DC',
+                  'icon': 'monument'
+                }
+              }, {
+                  'type': 'Feature',
+                  'geometry': {
+                    'type': 'Point',
+                    'coordinates': [-122.414, 37.776]
+                  },
+                  'properties': {
+                    'title': 'Mapbox SF',
+                    'icon': 'harbor'
+                  }
+                }, {
+                  'type': 'Feature',
+                  'geometry': {
+                    'type': 'Point',
+                    'coordinates': [-117.158633, 32.709384]
+                  },
+                  'properties': {
+                    'title': 'Fifty and Fifty',
+                    'icon': 'harbor'
+                  }
+                }]
+            }
+          },
+          'layout': {
+            'icon-image': '{icon}-15',
+            'text-field': '{title}',
+            'text-font': ['Open Sans Semibold', 'Arial Unicode MS Bold'],
+            'text-offset': [0, 0.6],
+            'text-anchor': 'top'
+          }
+        });
+      },
+      mapClicked(map, e){
+        console.log('Map Clicked!')
+      }
     }
   }
 </script>
@@ -22,10 +79,11 @@
 <template>
   <div id="map" class="map">
     <mapbox
+    @map-load="mapLoaded"
     access-token="pk.eyJ1IjoiZmlmdHlhbmRmaWZ0eSIsImEiOiJjajFxdjVibmswMGptMndyaW5vb2VoOHBsIn0.aD863YaLh6B8Mg2cRgdl1Q"
     :map-options="{
-    	style: 'mapbox://styles/fiftyandfifty/cj3656bvt000h2smugognpdh1',
-    	center: [-96, 37.8],
+      style: 'mapbox://styles/fiftyandfifty/cj3g6it5p00122rnyetxu6rrq',
+      center: [-122.420679, 37.772537],
     	zoom: 3
     }">
   </mapbox>
