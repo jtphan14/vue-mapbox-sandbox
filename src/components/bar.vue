ffdadec718601553a972eda579d6d76670c0e660
Jacqueline Phan <jtphan14@gmail.com>
2017-07-18 14:14:25 -0700

    Use vue-chartjs for graphsinstead

---
+++ src/components/bar.vue
@@ -0,0 +1,50 @@
+<script>
+//
+// import VueCharts from 'vue-chartjs'
+// import { HorizontalBar, Line, mixins } from 'vue-chartjs'
+// const { reactiveProp } = mixins
+//
+// export default HorizontalBar.extend({
+//   mounted () {
+//     // Overwriting base render method with actual data.
+//     this.renderChart({
+//       labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+//       datasets: [
+//         {
+//           label: 'GitHub Commits',
+//           backgroundColor: '#f87979',
+//           data: [40, 20, 12, 39, 10, 40, 39, 80, 40, 20, 12, 11]
+//         }
+//       ],
+//     })
+//   },
+//
+// });
+
+import { Bar, HorizontalBar } from 'vue-chartjs'
+
+export default HorizontalBar.extend({
+  data(){
+    return {
+      chartData: {
+        labels: ['January', 'February'],
+        datasets: [
+          {
+            label: 'GitHub Commits',
+            backgroundColor: '#f87979',
+            data: [40, 20]
+          }
+        ]
+      },
+      options: {
+        responsive: false,
+        maintainAspectRatio: false
+      }
+    }
+  },
+  mounted () {
+    // Overwriting base render method with actual data.
+    this.renderChart(this.chartData, this.options)
+  }
+})
+</script>
